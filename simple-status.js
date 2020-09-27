dc427cc38fed5e4487fa81a496873b387ffc68a7
Michal Czapracki <cz@signicode.com>
2020-09-27 21:17:20 +0000

    Update compatibility to scramjet 4.x

--- simple-status.js
+++ simple-status.js
@@ -21,10 +21,12 @@ if (options.port) port =  options.port;
 console.log("Crunching tweets for... ", cfg);
 
 const data = {};
+let i = 0;
 
 // eslint-disable-next-line node/no-unpublished-require
 new TwitJet(require("./twitter-config.json"))
     .statuses(cfg, "tweet")
+    .do(() => ++i%100 || console.log(`${i} twits processed`))
     .map((tweet) => Object.assign({
         text: options._.filter(
             (a) => tweet.text.toLowerCase().indexOf(a.toLowerCase()) >= 0
@@ -34,19 +36,26 @@ new TwitJet(require("./twitter-config.json"))
         const ret = a.hashtags
             .map(a => "#" + a.text)
             .filter(a => a)
-            .map(a => a.toLowerCase())
-            .concat(
+            .map(a => a.toLowerCase());
+        
+        if (options.mentions) 
+            return ret.concat(
                 (a.user_mentions || [])
                     .map(a => "@" + a.screen_name.toLowerCase())
             );
+        
         return ret;
     })
     .filter(a => !cfg.track || cfg.track.indexOf(a) === -1)
     .use((stream) => new scramjet.MultiStream([300, 900, 3600].map(
-        (span) =>
-            stream.pipe(mw.functions.topn(mw.FixedTimeWindow, {span: span * 1e3, n: 15}))
-                .pipe(new scramjet.DataStream())
-                .map((topn) => ({span: span, list: topn}))
+        (span) => {
+            const str = stream
+                .pipe(mw.functions.topn(mw.FixedTimeWindow, {span: span * 1e3, n: 15}))
+                .tap();
+            
+            return str.pipe(new scramjet.DataStream())
+                .map((topn) => ({span: span, list: topn}));
+        }
     )))
     .mux()
     .each((el) => {
